6e941b92b4e812d871ed266b79c2d8c28f58a4f5
zhuzhenxuan <piofcircle@gmail.com>
2019-01-06 02:14:15 +0800

    shebang

--- docker/arcface/run.sh
+++ docker/arcface/run.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 docker run --rm -ti -v /home/zzx/github.com/otrewyi191/insightface/models/model-r100-ii/:/model \
 	-v '/home/zzx/facedata/photo_2019-01-05_13-48-45-2.jpg':/img.jpg \
 	arcface bash
