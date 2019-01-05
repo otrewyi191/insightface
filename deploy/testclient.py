e2519d3c534e297f4daadf8e16f05f83a022dd7d
zhuzhenxuan <piofcircle@gmail.com>
2019-01-06 01:53:22 +0800

    predict ok

---
+++ deploy/testclient.py
@@ -0,0 +1,60 @@
+import argparse
+import os
+
+import cv2
+import grpc
+import numpy as np
+
+_HOST = '127.0.0.1'
+_PORT = '8080'
+
+import face_model
+
+from grpcserver import facenet_pb2_grpc, facenet_pb2
+_ONE_DAY_IN_SECONDS = 60 * 60 * 24
+_HOST = '127.0.0.1'
+_PORT = '8080'
+
+parser = argparse.ArgumentParser(description='face model test')
+# general
+parser.add_argument('--image-size', default='112,112', help='')
+parser.add_argument('--model', default='', help='path to load model.')
+parser.add_argument('--ga-model', default='', help='path to load model.')
+parser.add_argument('--gpu', default=0, type=int, help='gpu id')
+parser.add_argument('--det', default=0, type=int, help='mtcnn option, 1 means using R+O, 0 means detect from begining')
+parser.add_argument('--flip', default=0, type=int, help='whether do lr flip aug')
+parser.add_argument('--threshold', default=1.24, type=float, help='ver dist threshold')
+args = parser.parse_args()
+
+model = face_model.FaceModel(args)
+
+
+def get_numpy_from_img(img_path):
+    img = cv2.imread(img_path)
+    img = model.get_input(img)
+    return img
+
+
+
+
+def get_input_from_img(img_path):
+    img = cv2.imread(img_path)
+    img = model.get_input(img)
+
+def run():
+    conn = grpc.insecure_channel(_HOST + ':' + _PORT)
+    client = facenet_pb2_grpc.GetEmbeddingStub(channel=conn)
+    image = get_numpy_from_img(os.getenv('IMGPATH', '/home/zzx/facedata/photo_2019-01-05_12-43-40.jpg'))
+
+    shape = image.shape
+
+    image_message = facenet_pb2.ImageMessage(image=image.tobytes(), dim=list(shape))
+
+
+    response = client.Get(image_message)
+    print("received: ")
+    print(response)
+
+
+if __name__ == '__main__':
+    run()
---
+++ deploy/testserver.py
@@ -0,0 +1,61 @@
+import argparse
+import os
+import time
+
+import cv2
+import grpc
+import numpy as np
+from concurrent import futures
+
+import face_model
+
+from grpcserver import facenet_pb2_grpc, facenet_pb2
+_ONE_DAY_IN_SECONDS = 60 * 60 * 24
+_HOST = '127.0.0.1'
+_PORT = '8080'
+
+parser = argparse.ArgumentParser(description='face model test')
+# general
+parser.add_argument('--image-size', default='112,112', help='')
+parser.add_argument('--model', default='', help='path to load model.')
+parser.add_argument('--ga-model', default='', help='path to load model.')
+parser.add_argument('--gpu', default=0, type=int, help='gpu id')
+parser.add_argument('--det', default=0, type=int, help='mtcnn option, 1 means using R+O, 0 means detect from begining')
+parser.add_argument('--flip', default=0, type=int, help='whether do lr flip aug')
+parser.add_argument('--threshold', default=1.24, type=float, help='ver dist threshold')
+args = parser.parse_args()
+
+model = face_model.FaceModel(args)
+
+class FormatData(facenet_pb2_grpc.GetEmbeddingServicer):
+    def Get(self, request, context):
+
+        # parse image to numpy array
+        embedding = request.image
+        nparr = np.frombuffer(embedding, dtype=np.uint8)
+        shape = list(request.dim)
+
+        reshape = nparr.reshape(shape)
+        print(reshape)
+        print(reshape.shape)
+
+        # predict
+        feature = model.get_feature(reshape)
+        print(feature)
+        return facenet_pb2.EmbeddingMessage()
+
+
+def serve():
+    grpcServer = grpc.server(futures.ThreadPoolExecutor(max_workers=4))
+    facenet_pb2_grpc.add_GetEmbeddingServicer_to_server(FormatData(), grpcServer)
+    grpcServer.add_insecure_port(_HOST + ':' + _PORT)
+    grpcServer.start()
+    try:
+        while True:
+            time.sleep(_ONE_DAY_IN_SECONDS)
+    except KeyboardInterrupt:
+        grpcServer.stop(0)
+
+
+if __name__ == '__main__':
+    serve()
