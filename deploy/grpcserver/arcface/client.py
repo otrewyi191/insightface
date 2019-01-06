267ed066d966fd6b3e661d18313c55d437bf44eb
zhuzhenxuan <piofcircle@gmail.com>
2019-01-06 12:46:20 +0800

    server need --model

--- deploy/grpcserver/arcface/client.py
+++ deploy/grpcserver/arcface/client.py
@@ -1,26 +1,41 @@
-# ! /usr/bin/env python
-# -*- coding: utf-8 -*-
+import argparse
+import os
+
 import cv2
-
 import grpc
-import facenet_pb2,facenet_pb2_grpc
 import numpy as np
-
 
 _HOST = '127.0.0.1'
 _PORT = '8080'
 
-def get_str():
+import face_model
 
-    # Create a dummy matrix
-    img = np.ones((50, 50, 3), dtype=np.uint8) * 255
-    # Save the shape of original matrix.
-    img_shape = img.shape
+from grpcserver.arcface import facenet_pb2_grpc, facenet_pb2
 
-    message_image = img.tobytes()
-    shape = img.shape
-    # message_image = np.ndarray.tostring(img)
-    return message_image,shape
+_ONE_DAY_IN_SECONDS = 60 * 60 * 24
+_HOST = '127.0.0.1'
+_PORT = '8080'
+
+parser = argparse.ArgumentParser(description='face model test')
+# general
+parser.add_argument('--image-size', default='112,112', help='')
+parser.add_argument('--model', default='', help='path to load model.')
+parser.add_argument('--ga-model', default='', help='path to load model.')
+parser.add_argument('--gpu', default=0, type=int, help='gpu id')
+parser.add_argument('--det', default=0, type=int, help='mtcnn option, 1 means using R+O, 0 means detect from begining')
+parser.add_argument('--flip', default=0, type=int, help='whether do lr flip aug')
+parser.add_argument('--threshold', default=1.24, type=float, help='ver dist threshold')
+args = parser.parse_args()
+
+model = face_model.FaceModel(args)
+
+
+def get_numpy_from_img(img_path):
+    img = cv2.imread(img_path)
+    img = model.get_input(img)
+    return img
+
+
 
 
 def get_input_from_img(img_path):
@@ -30,14 +45,23 @@ def get_input_from_img(img_path):
 def run():
     conn = grpc.insecure_channel(_HOST + ':' + _PORT)
     client = facenet_pb2_grpc.GetEmbeddingStub(channel=conn)
+    image = get_numpy_from_img(os.getenv('IMGPATH', '/home/zzx/facedata/photo_2019-01-05_12-43-40.jpg'))
 
-    image,shape = get_str()
-    image_message = facenet_pb2.ImageMessage(image=image, dim=[50, 50, 3])
+    shape = image.shape
+
+    image_message = facenet_pb2.ImageMessage(image=image.tobytes(), dim=list(shape))
 
 
     response = client.Get(image_message)
-    print("received: ")
-    print(response)
+
+    embedding = response.embedding
+    embedding = np.frombuffer(embedding,dtype=np.float32)
+    embedding = embedding.reshape(list(response.dim))
+
+
+    print("embedding: ")
+    print(embedding)
+    print(embedding.shape)
 
 
 if __name__ == '__main__':
--- deploy/grpcserver/arcface/server.py
+++ deploy/grpcserver/arcface/server.py
@@ -1,15 +1,30 @@
-#! /usr/bin/env python
-# -*- coding: utf-8 -*-
-import grpc
+import argparse
 import time
-from concurrent import futures
-import facenet_pb2,facenet_pb2_grpc
+
+import grpc
 import numpy as np
+from concurrent import futures
+
+import face_model
+
+from grpcserver.arcface import facenet_pb2_grpc, facenet_pb2
 
 _ONE_DAY_IN_SECONDS = 60 * 60 * 24
-_HOST = '127.0.0.1'
+_HOST = '0.0.0.0'
 _PORT = '8080'
 
+parser = argparse.ArgumentParser(description='face model test')
+# general
+parser.add_argument('--image-size', default='112,112', help='')
+parser.add_argument('--model', default='', help='path to load model.')
+parser.add_argument('--ga-model', default='', help='path to load model.')
+parser.add_argument('--gpu', default=0, type=int, help='gpu id')
+parser.add_argument('--det', default=0, type=int, help='mtcnn option, 1 means using R+O, 0 means detect from begining')
+parser.add_argument('--flip', default=0, type=int, help='whether do lr flip aug')
+parser.add_argument('--threshold', default=1.24, type=float, help='ver dist threshold')
+args = parser.parse_args()
+
+model = face_model.FaceModel(args)
 
 class FormatData(facenet_pb2_grpc.GetEmbeddingServicer):
     def Get(self, request, context):
@@ -24,7 +39,11 @@ class FormatData(facenet_pb2_grpc.GetEmbeddingServicer):
         print(reshape.shape)
 
         # predict
-        return facenet_pb2.EmbeddingMessage()
+        feature = model.get_feature(reshape)
+        print(feature)
+
+        embedding_message = facenet_pb2.EmbeddingMessage(embedding=feature.tobytes(), dim=(feature.shape))
+        return embedding_message
 
 
 def serve():
